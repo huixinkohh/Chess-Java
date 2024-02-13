8d51792851bc28c7a5b3214f622795c8eaaa2cae
huixinkohh <hui.koh-2@student.manchester.ac.uk>
2024-02-13 01:28:40 +0000

    Chess

---
+++ src/main/Main.java
@@ -0,0 +1,24 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		JFrame window = new JFrame("Chess by Hui Koh");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		
+		GamePanel gp = new GamePanel();
+		window.add(gp);
+		window.pack();
+		
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
+		
+		gp.launchGame();
+	}
+	
+
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Chess {
+	requires java.desktop;
+}
+
